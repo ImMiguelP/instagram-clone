c8e0999f5902757b6d42af532c209a186b624fc1
ImLeugim <anarchxzx@gmail.com>
2022-06-14 13:39:44 -0700

    Merge branch 'master' of https://github.com/ImMiguelP/instagram-clone

--- src/Pages/SupaBaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://tgudrycpfzttxzcdwpym.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRndWRyeWNwZnp0dHh6Y2R3cHltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM5NDgzODQsImV4cCI6MTk2OTUyNDM4NH0.VOBxjXrgRnUb8jIScbcKh_KDvCnYhxXpAlyPMGzwKMI"
-);
